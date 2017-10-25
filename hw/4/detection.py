a53ab3dcc1d50a8da33a2186843b69cbf7d8fbc7
Alex Zabelin <alex--zabelin@mail.ru>
2017-10-25 10:20:26 +0000

    remove X_test modiving

--- hw/4/detection.py
+++ hw/4/detection.py
@@ -235,7 +235,6 @@ def train_detector(train_gt, train_img_dir, fast_train, validation=0.0):
                                  col_shift_range=10,
                                  horizontal_flip=True,)
     datagen.fit(X_train)
-    X_test = datagen.standartize(X_test)
 
     for i in range(epochs):
         if not fast_train:
@@ -258,10 +257,6 @@ def train_detector(train_gt, train_img_dir, fast_train, validation=0.0):
 
 def detect(model, test_img_dir):
     X_test, sizes, file_names = _get_images_from_directory(test_img_dir)
-    datagen = ImageDataGenerator(featurewise_center=True,
-                                 featurewise_std_normalization=True)
-    datagen.fit(X_test)
-    X_test = datagen.standartize(X_test)
     answers = model.predict(X_test, batch_size=128)
     answers = _rescale_answers(answers, sizes, False)
     answers = {file_name: answer
